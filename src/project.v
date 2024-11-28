0f184beb04121c7a20cd79fb5f1156c882a08cab
wakiyayuto <t322083@m.chukyo-u.ac.jp>
2024-11-28 14:24:12 +0900

    Update project.v

--- src/project.v
+++ src/project.v
@@ -31,7 +31,7 @@ module tt_um_example (
 ///////////////////////
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out[7:2]= 0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
